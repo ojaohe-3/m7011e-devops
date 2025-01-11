bc13d073d82fb79e86a5f79788b2bab4f9d333d3
johan <itgsod.johan.holmgren@gmail.com>
2025-01-11 18:01:11 +0100

    improving the setup

--- local-app/imports/createDatabases.sql
+++ local-app/imports/createDatabases.sql
@@ -13,21 +13,6 @@ CREATE DATABASE "core-app"
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
--- Database: keycloak
-
--- DROP DATABASE IF EXISTS keycloak;
-
-CREATE DATABASE keycloak
-    WITH
-    OWNER = keycloak
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
 -- Database: registry
 
 -- DROP DATABASE IF EXISTS registry;
@@ -70,5 +55,19 @@ CREATE ROLE keycloak WITH
   CREATEROLE
   NOREPLICATION
   BYPASSRLS
-  ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:bNsZXzv4OQWJ6lnwaMzCOQ==$VqRYXqG9mCy3eGvkMJ1fsNWjI54HjwquqCqrYEJBQ6s=:ypm6s/hdt/iU1Y2t1TCgm/vDB7rq3HI5cqTDbwFPm3M='; 
+  ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:bNsZXzv4OQWJ6lnwaMzCOQ==$VqRYXqG9mCy3eGvkMJ1fsNWjI54HjwquqCqrYEJBQ6s=:ypm6s/hdt/iU1Y2t1TCgm/vDB7rq3HI5cqTDbwFPm3M=';
 
+-- Database: keycloak
+
+-- DROP DATABASE IF EXISTS keycloak;
+
+CREATE DATABASE keycloak
+    WITH
+    OWNER = keycloak
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
